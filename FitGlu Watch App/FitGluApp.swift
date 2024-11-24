edb474be23c06c5e49dc72ba6f361e6c6edf1887
Alexander Rusak <a.rusak@sweedpos.com>
2024-11-24 13:25:11 +0100

    Initial Commit

---
+++ FitGlu Watch App/FitGluApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitGluApp.swift
+//  FitGlu Watch App
+//
+//  Created by Александр Русак on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitGlu_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitGlu Watch AppTests/FitGlu_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitGlu_Watch_AppTests.swift
+//  FitGlu Watch AppTests
+//
+//  Created by Александр Русак on 24/11/2024.
+//
+
+import Testing
+@testable import FitGlu_Watch_App
+
+struct FitGlu_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
